d58e1613fd5f5bd1688dea45a6e5b5154bb17699
Bhimanagouda-4929 <147364564+Bhimanagouda-4929@users.noreply.github.com>
2023-11-05 11:17:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,226 @@
+const data= [
+    {
+        id : 0,
+        img : 'images/redmiK20.jpg',
+        name : 'Redmi K20',
+        price : 25000,
+        save : 2500,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 1,
+        img : 'images/samGalaxynote20.jpg',
+        name : 'Samsung Galaxy Note 20',
+        price : 50000,
+        save : 2500,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 2,
+        img : 'images/oppofindX2.jpg',
+        name : 'OPPO Find X2',
+        price : 24000,
+        save : 1000,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 3,
+        img : 'images/realmeX20pro.jpg',
+        name : 'Realme X50 Pro',
+        price : 28500,
+        save : 1000,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 4,
+        img : 'images/redminote8.jpg',
+        name : 'Redmi Note 8',
+        price : 20000,
+        save : 1500,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 5,
+        img : 'images/redminote9.jpg',
+        name : 'Redmi Note 9',
+        price : 22000,
+        save : 2500,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 6,
+        img : 'images/redmi8.jpg',
+        name : 'Redmi 8A Dual',
+        price : 16000,
+        save : 2000,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+    {
+        id : 7,
+        img : 'images/redmi9.jpg',
+        name : 'Redmi 9',
+        price : 10000,
+        save : 1000,
+        delievery : 'In 3 - 4 days',
+        itemInCart: false
+    },
+];
+
+let cartList=[]; 
+
+var i;
+var detail =document.getElementsByClassName('card-item');
+var detailsImg = document.getElementById('details-img')
+var detailTitle = document.getElementById('detail-title')
+var detailPrice = document.getElementById('detail-price')
+var youSave = document.getElementById('you-save');
+var detailsPage = document.getElementById('details-page');
+var back = document.getElementById('buy')
+back.addEventListener('click',refreshPage)
+var addToCarts = document.querySelectorAll('#add-to-cart')
+var cart = document.getElementById('cart');
+
+cart.addEventListener('click',displayCart)
+
+var carts = document.getElementById('carts');
+
+carts.addEventListener('click',()=>addToCart(getId))
+
+var home = document.getElementById('logo');
+
+home.addEventListener('click',hideCart);
+
+document.addEventListener('click',function (e){
+    if(e.target.id=='remove'){
+        var itemId = e.target.parentNode.id
+        removeFromCart(itemId)
+    }
+})
+
+for(i=0;i<data.length;i++){
+    detail[i].addEventListener('click',handleDetail)
+}
+
+var getId;
+
+addToCarts.forEach(val=>val.addEventListener('click',()=>addToCart(val.parentNode.id)));
+
+function handleDetail(e){
+    detailsPage.style.display = 'block'
+    getId= this.parentNode.id;
+    detailsImg.src= data[getId].img;
+    detailTitle.innerHTML=   data[getId].name;
+    detailPrice.innerHTML= 'Price : RS ' +data[getId].price;
+    youSave.innerHTML= 'You save : (RS ' + data[getId].save + ')';
+}
+
+function addToCart(id) {
+    if(!data[id].itemInCart){
+        cartList= [...cartList,data[id]];
+        addItem()
+        
+        alert('item added to your cart')
+
+    }
+    else{
+        alert('your item is already there')
+    }
+    data[id].itemInCart= true
+}
+
+function refreshPage(){
+    detailsPage.style.display = 'none'
+}
+
+function hideCart(){
+    document.getElementById('main').style.display= "block";
+    document.getElementById('cart-container').style.display= "none";
+}
+
+function displayCart(){
+    document.getElementById('main').style.display= "none";
+    document.getElementById('details-page').style.display= "none";
+    document.getElementById('cart-container').style.display= "block";
+    if(cartList.length==0){
+        document.getElementById('cart-with-items').style.display= "none";
+        document.getElementById('empty-cart').style.display= "block";
+    }
+    else{
+        document.getElementById('empty-cart').style.display= "none";
+        document.getElementById('cart-with-items').style.display= "block";
+        
+    }
+}
+
+var totalAmount;
+var totalItems;
+var totalSaving;
+
+function addItem(){
+    totalAmount=0;
+    totalItems = 0;
+    totalSaving=0
+    var clrNode=document.getElementById('item-body');
+        clrNode.innerHTML= '';
+        console.log(clrNode.childNodes)
+        cartList.map((cart)=>
+        {
+            var cartCont = document.getElementById('item-body');
+            totalAmount = totalAmount + cart.price;
+            totalSaving = totalSaving + cart.save;
+            totalItems = totalItems + 1;
+
+            var tempCart = document.createElement('div')
+            tempCart.setAttribute('class','cart-list');
+            tempCart.setAttribute('id',cart.id);
+
+            var listImg = document.createElement('img');
+            listImg.setAttribute('id','list-img');
+            listImg.src = cart.img
+            tempCart.appendChild(listImg)
+
+            var listName = document.createElement('h3');
+            listName.setAttribute('class','list-name');
+            listName.innerHTML = cart.name;
+            tempCart.appendChild(listName)
+
+            var listPay = document.createElement('h3');
+            listPay.setAttribute('class','pay');
+            listPay.innerHTML = cart.price;
+            tempCart.appendChild(listPay);
+
+            var listQuantity = document.createElement('h3');
+            listQuantity.setAttribute('class','quantity');
+            listQuantity.innerHTML = '1';
+            tempCart.appendChild(listQuantity);
+
+            var listTrash = document.createElement('i');
+            listTrash.setAttribute('class','fa fa-trash ');
+            listTrash.setAttribute('id','remove');
+            tempCart.appendChild(listTrash);
+
+            cartCont.appendChild(tempCart)
+            
+        })
+        document.getElementById('total-amount').innerHTML = 'Total Amount : RS ' + totalAmount;
+        document.getElementById('total-items').innerHTML = 'Total Items : ' + totalItems;
+        document.getElementById('you-saved').innerHTML = 'You Saved : RS ' + totalSaving;
+        document.getElementById('total').style.display= "block";
+}
+
+function removeFromCart(itemId){
+    data[itemId].itemInCart = false
+    cartList = cartList.filter((list)=>list.id!=itemId);
+    addItem()
+    if(cartList.length==0){
+        document.getElementById('cart-with-items').style.display= "none";
+        document.getElementById('empty-cart').style.display= "block";
+    }
+}
